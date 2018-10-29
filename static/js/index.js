df919c9a14d9224e9a4ed31a39c025541581b82d
Oliver Elgaard Gellett <olivergellett@Olivers-MacBook-Pro.local>
2018-10-29 08:54:19 +0100

    Opdatering

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,12 @@
 $(function(){
 
-    if (sessionStorage.getItem('cookie') == undefined) {
+    if (localStorage.getItem('cookie') == undefined) {
         $("#cookies").animate({bottom: "0px"});
     }
 
     $("#okay").on('click', function() {
         $("#cookies").animate({bottom: "-171px"});
-        sessionStorage.setItem('cookie', '1');
+        localStorage.setItem('cookie', '1');
     });
+
 });
